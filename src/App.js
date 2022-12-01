bc8966313b163e312a687d00d01ffa60cebcf1e8
Air <samuelnegalign1@gmail.com>
2022-12-01 13:24:39 +0300

    correcting user for third time

--- src/App.js
+++ src/App.js
@@ -25,8 +25,8 @@ function App() {
  //const admin = false;
  //console.log("user",user.currentUser.isAdmin)
  useEffect(()=>{
-  console.log("Data",JSON.parse(localStorage.getItem("persist:root")));
-  //setAdmin(JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.isAdmin)
+  console.log("Data",JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user));
+  setAdmin(JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.isAdmin)
  },[admin])
  console.log("user",admin)
   return (
